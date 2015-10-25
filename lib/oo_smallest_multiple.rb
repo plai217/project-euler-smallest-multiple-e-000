880f61922a2f1b2c86e8f91176d541110baee5f0
plai217 <philiplai217@gmail.com>
2015-10-24 22:58:21 -0400

    Done.

--- lib/oo_smallest_multiple.rb
+++ lib/oo_smallest_multiple.rb
@@ -1,1 +1,14 @@
 # Implement your object-oriented solution here!
+class SmallestMultiple
+
+  def initialize(num)
+    @num = num
+  end
+
+  def lcm
+    array = 1.upto(@num).map {|x| x}.reverse
+    array.reduce(:lcm)
+  end
+
+
+end
--- lib/smallest_multiple.rb
+++ lib/smallest_multiple.rb
@@ -1,1 +1,9 @@
 # Implement your procedural solution here!
+
+#finds lowest multiple for two numbers
+
+def smallest_multiple(num)
+  array = 1.upto(num).map {|x| x}.reverse
+  array.reduce(:lcm)
+end
+
